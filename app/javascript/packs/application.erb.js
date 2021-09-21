882c76f681a0416e684e4d493690c67e24fe32ac
Jack Ewing <jackewing23@gmail.com>
2021-09-21 13:17:49 +1200

    rip out old w2n add video

--- app/javascript/packs/application.erb.js
+++ app/javascript/packs/application.erb.js
@@ -12,18 +12,6 @@ Rails.start();
 Turbolinks.start();
 ActiveStorage.start();
 
-function importAll(r) {
-  return r.keys().map(r);
-}
-
-const allImages = importAll(
-  require.context(
-    "../../assets/images/img_sequence/",
-    false,
-    /\.(png|jpe?g|svg)$/
-  )
-);
-
 document.addEventListener("turbolinks:load", () => {
   if (document.getElementsByClassName("hand").length) {
     if (interval) {
@@ -43,68 +31,4 @@ document.addEventListener("turbolinks:load", () => {
         element.classList.toggle("open");
       });
     });
-
-  // var initHeight = window.innerHeight;
-  if (document.querySelector(".wtn")) {
-    setTimeout(() => {
-      document.getElementById("cover").classList.add("hidden");
-    }, 500);
-
-    var totalImages = allImages.length;
-    var images = [];
-    for (var i = 1; i < totalImages; i++) {
-      var img = new Image();
-      img.src = allImages[i];
-      images.push(img);
-    }
-    var canv = document.getElementById("background");
-    var splashHeight =
-      document.getElementsByClassName("splash")[0].clientHeight;
-    var context = canv.getContext("2d");
-
-    var currentLocation = 0;
-
-    var setImage = function (newLocation) {
-      context.drawImage(images[newLocation], 0, 0, 1920, 1080);
-    };
-
-    var MouseWheelHandler = function (e) {
-      var percent = window.scrollY / splashHeight;
-      currentLocation = Math.round(totalImages * percent);
-      // See below for the details of this function
-      setImage(currentLocation);
-    };
-    var canvasFillWin = function (e) {
-      // var h = 720;
-      // var w = 1280;
-      // var ratio = h / w;
-      // var winW = $(window).width();
-      // var winH = $(window).height();
-      // var winRatio = winH / winW;
-      // if (winRatio > ratio) {
-      //   $(canv)
-      //     .width(winH / ratio)
-      //     .height(winH)
-      //     .css({
-      //       marginLeft: -winH / ratio / 2 + "px",
-      //       left: "50%",
-      //       top: "0",
-      //       marginTop: "0",
-      //     });
-      // } else {
-      //   $(canv)
-      //     .width(winW)
-      //     .height(winW * ratio)
-      //     .css({
-      //       marginLeft: "0",
-      //       left: "0",
-      //       top: "50%",
-      //       marginTop: (-winW * ratio) / 2 + "px",
-      //     });
-      // }
-    };
-    window.addEventListener("scroll", MouseWheelHandler, false);
-    setImage(1);
-    canvasFillWin();
-  }
 });
